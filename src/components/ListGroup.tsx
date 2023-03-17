411280ffdbc023c0f2f35f898ede726e3d27a2ea
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-17 10:56:08 +0700

    List Group Component

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ListGroup = () => {
+  return (
+    <div>
+      <ul className="list-group">
+        <li className="list-group-item">List 1</li>
+        <li className="list-group-item">List 2</li>
+        <li className="list-group-item">List 3</li>
+        <li className="list-group-item">List 4</li>
+        <li className="list-group-item">List 5</li>
+      </ul>
+    </div>
+  );
+};
+
+export default ListGroup;
