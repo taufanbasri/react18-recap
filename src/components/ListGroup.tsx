db099b7ce3a4550a25d6172a160ec253711abc37
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-17 11:18:59 +0700

    Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -6,6 +6,9 @@ const ListGroup = () => {
   return (
     <>
       <h1>List</h1>
+
+      {items.length === 0 && <p>No item found!</p>}
+
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">
