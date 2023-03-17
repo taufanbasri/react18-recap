29485b035e9854a5e23830d9fc44290ef59575ab
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-17 14:59:01 +0700

    Showing Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisible] = useState(false);
+
   return (
     <div>
-      <Button onClick={() => console.log("Clicked")}>Default</Button>
-      <Button color="primary" onClick={() => console.log("Clicked")}>
-        Primary
-      </Button>
-      <Button color="danger" onClick={() => console.log("Clicked")}>
-        Danger
-      </Button>
-      <Button color="secondary" onClick={() => console.log("Clicked")}>
-        Secondary
-      </Button>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>
+      )}
+      <Button onClick={() => setAlertVisible(true)}>My Button</Button>
     </div>
   );
 }
