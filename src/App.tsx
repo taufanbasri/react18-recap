5746ae7052e3593eef02b79150254febed722ef1
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-18 21:16:01 +0700

    Exercise - Updating State3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
+
+    console.log(cart);
   };
 
   return (
