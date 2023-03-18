8150a1ba05b71bd58c220334098177598b810904
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-18 23:24:52 +0700

    Handle Form Submission

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import { FormEvent } from "react";
 
 const Form = () => {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log("Submitted");
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
