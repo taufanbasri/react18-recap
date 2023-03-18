c0e69acf56aa15751cbf1552a0fdb6647b0d97e8
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-18 14:14:40 +0700

    Exercise - Updating State

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John",
+    },
+  });
+
+  const handleClick = () => {
+    setGame({ ...game, player: { ...game.player, name: "Taufan" } });
+  };
+
+  return (
+    <div>
+      <p>{game.player.name}</p>
+      <button onClick={handleClick}>Click Me</button>
+    </div>
+  );
 }
 
 export default App;
