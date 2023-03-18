a64e4b289fbb5ed34281253a54498d8b7c283dc9
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-18 13:36:58 +0700

    Cleans up

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,5 @@
-import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
-
 function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
-
-  return (
-    <div>
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
