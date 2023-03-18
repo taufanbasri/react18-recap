ec0c1c8dc43899a54187138874abeb535970161e
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-18 21:33:43 +0700

    Exercise - Expandable Text

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import ExpandableText from "./components/ExpandableText";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <ExpandableText maxChars={10}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
+        repellendus quae sapiente, natus aliquam nostrum architecto inventore
+        tempora voluptatum ut ipsam magnam, quo temporibus deleniti non
+        accusantium placeat at quis perspiciatis explicabo excepturi quaerat
+        possimus sunt iusto! Nihil eum sequi blanditiis hic ratione dolorum
+        delectus, voluptatum facere. Mollitia debitis nam perferendis pariatur
+        ut ex sequi! Debitis est tempore hic quaerat! Quasi dolor repellat culpa
+        eaque possimus quo totam modi impedit ad optio atque facere quos in
+        architecto temporibus sed corporis voluptas velit mollitia vel, tenetur
+        neque labore dolore suscipit? Quia consequatur porro, aliquam
+        necessitatibus consectetur aperiam consequuntur neque id illum?
+      </ExpandableText>
+    </div>
+  );
 }
 
 export default App;
