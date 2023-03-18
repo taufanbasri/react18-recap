3c35e00ab2a62f8416383163e90c6f097beb34aa
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-18 23:01:01 +0700

    Build Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText maxChars={10}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem
-        repellendus quae sapiente, natus aliquam nostrum architecto inventore
-        tempora voluptatum ut ipsam magnam, quo temporibus deleniti non
-        accusantium placeat at quis perspiciatis explicabo excepturi quaerat
-        possimus sunt iusto! Nihil eum sequi blanditiis hic ratione dolorum
-        delectus, voluptatum facere. Mollitia debitis nam perferendis pariatur
-        ut ex sequi! Debitis est tempore hic quaerat! Quasi dolor repellat culpa
-        eaque possimus quo totam modi impedit ad optio atque facere quos in
-        architecto temporibus sed corporis voluptas velit mollitia vel, tenetur
-        neque labore dolore suscipit? Quia consequatur porro, aliquam
-        necessitatibus consectetur aperiam consequuntur neque id illum?
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
