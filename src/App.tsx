f6652c76b268d1324272038869b2c93183bb6dca
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-18 13:02:38 +0700

    Update State Object with immer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
-import Like from "./components/Like";
+import produce from "immer";
+import { useState } from "react";
 
 function App() {
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "Bug 1", fixed: false },
+    { id: 2, title: "Bug 2", fixed: false },
+  ]);
+
+  const handleClick = () => {
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+
+        if (bug) bug.fixed = true;
+      })
+    );
+  };
+
   return (
     <div>
-      <Like onClick={() => console.log("clicked")} />
+      {bugs.map((bug) => (
+        <p key={bug.id}>
+          {bug.title} {bug.fixed ? "Fixed" : "New"}
+        </p>
+      ))}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
