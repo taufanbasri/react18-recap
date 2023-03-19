3ddcff5fb224ff98c38806cf64fb9e42422bdecc
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-20 03:08:58 +0700

    useEffect Fetch Data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
+
 function App() {
-  return <div></div>;
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data));
+  }, []);
+
+  return (
+    <ol>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ol>
+  );
 }
 
 export default App;
