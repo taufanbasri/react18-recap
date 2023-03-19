ed2f6cbca63d494537479562026dd95141cc057a
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-20 02:47:46 +0700

    Cleans up

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,5 @@
-import { useState } from "react";
-import ProductList from "./components/ProductList";
-
 function App() {
-  const [category, setCategory] = useState("");
-
-  return (
-    <div>
-      <select
-        className="form-select"
-        onChange={(e) => setCategory(e.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category} />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
