1405193567eb53407819e326c813b17da667cad2
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-20 03:01:00 +0700

    Cleans up & install axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,4 @@
-import { useEffect } from "react";
-
-const connect = () => console.log("Connecting");
-const disconnect = () => console.log("Disconnecting");
-
 function App() {
-  useEffect(() => {
-    connect();
-
-    return () => disconnect();
-  });
-
   return <div></div>;
 }
 
