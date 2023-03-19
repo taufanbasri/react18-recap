35eea0de23a63f98ec6e23d02baa6ac3e767133a
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-20 00:57:02 +0700

    Project Expense Tracker - Integrating with React Hook From & Zod

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 
-export const categories = ["Groceries", "Utilities", "Entertainment"];
-
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
