72d7cb39c712ed98ce747a838e5c3aaeccb698d7
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-20 02:53:53 +0700

    useEffect Clean Up

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
+const connect = () => console.log("Connecting");
+const disconnect = () => console.log("Disconnecting");
+
 function App() {
+  useEffect(() => {
+    connect();
+
+    return () => disconnect();
+  });
+
   return <div></div>;
 }
 
