55436e39f0ab31a5a9c1a1bb48180ffc6c397557
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-20 00:00:32 +0700

    Project Expense Tracker - Build ExpenseForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+
+export const categories = ["Groceries", "Utilities", "Entertainment"];
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -17,6 +20,10 @@ function App() {
 
   return (
     <div>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
+
       <div className="mb-3">
         <ExpenseFilter
           onSelectCategory={(category) => setSelectedCategory(category)}
