f0a96044edc04e24543bc959fae092cc3c85da7b
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-20 08:16:57 +0700

    Delete Data

--- src/App.tsx
+++ src/App.tsx
@@ -33,15 +33,38 @@ function App() {
     return () => controller.abort();
   }, []);
 
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    axios
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
 
       {isLoading && <div className="spinner-border"></div>}
 
-      <ol>
+      <ol className="list-group">
         {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ol>
     </>
