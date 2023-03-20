c8afc6e5a8130869aba29ce8805a251d438fd283
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-03-20 07:10:24 +0700

    useEffect: Show loding indicator

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,25 @@ interface User {
 function App() {
   const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
 
+    setIsLoading(true);
     axios
       .get<User[]>("https://jsonplaceholder.typicode.com/users", {
         signal: controller.signal,
       })
-      .then((res) => setUsers(res.data))
+      .then((res) => {
+        setUsers(res.data);
+        setIsLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
 
         setError(err.message);
+        setIsLoading(false);
       });
 
     return () => controller.abort();
@@ -30,6 +36,9 @@ function App() {
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
+
+      {isLoading && <div className="spinner-border"></div>}
+
       <ol>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
